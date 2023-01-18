800207d826472bfc37040fadeb1e7363ae2472f0
annyeong-habo <jhb990438@gmail.com>
2023-01-18 16:57:34 +0900

    Initial commit

---
+++ src/main/java/com/springboot/api/controller/PutController.java
@@ -0,0 +1,36 @@
+package com.springboot.api.controller;
+
+import com.springboot.api.dto.MemberDto;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.Objects;
+
+@RestController
+@RequestMapping(value = "/api/v1/put-api")
+public class PutController {
+
+    @PutMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> putData){
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+        return sb.toString();
+    }
+
+    @PutMapping(value = "/member1")
+    public String postMemberDto1(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+    @PutMapping(value = "/member2")
+    public MemberDto postMemberDto2(@RequestBody MemberDto memberDto) {
+        return memberDto;
+    }
+
+
+}
